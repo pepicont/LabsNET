2d5c66664653f03835d4960e9a248c82e15383b0
pepicont <stefano.conti2004@gmail.com>
2025-04-14 18:41:30 -0300

    Terminé el 3, faltan todos los del 4

---
+++ Unidad02/Capitulo01/Iteracion/Program.cs
@@ -0,0 +1,12 @@
+﻿// See https://aka.ms/new-console-template for more information
+string[] arrayString; 
+arrayString = new string[5];
+int cantIteraciones = 5;
+for (int i = 0; i < cantIteraciones; i++)
+{
+    Console.WriteLine($"\nstring {i+1}: ");
+    arrayString[i] = Console.ReadLine();
+}
+for (int i = cantIteraciones-1; i >= 0; i--) {
+    Console.WriteLine($"\nEn la pos{i+1}: {arrayString[i]}");
+}
