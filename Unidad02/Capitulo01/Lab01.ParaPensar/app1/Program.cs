0b93a4efa2fea717ba7691fe3f7eb9a194e4657a
pepicont <stefano.conti2004@gmail.com>
2025-04-19 17:24:26 -0300

    Terminé app1 de para pensar

---
+++ Unidad02/Capitulo01/Lab01.ParaPensar/app1/Program.cs
@@ -0,0 +1,7 @@
+﻿https://aka.ms/new-console-template for more information
+Console.WriteLine("Ingrese el primer valor para la suma: ");
+int nro1 = int.Parse(Console.ReadLine() ?? "0"); //parse convierte el string en int
+Console.WriteLine("Ingrese el segundo valor: ");
+int nro2 = int.Parse(Console.ReadLine() ?? "0"); // ?? "0". Si ingresa null, se lee 0
+int suma = nro1 + nro2;
+Console.WriteLine($"El resultado de la suma de {nro1} y {nro2} es: {suma}");
