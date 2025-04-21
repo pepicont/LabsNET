a0026a067cfd1ec311864a673a7fc8abea54b5f2
pepicont <stefano.conti2004@gmail.com>
2025-04-21 13:06:42 -0300

    terminé unidad02/capitulo1

--- Unidad02/Capitulo01/Lab01.ParaPensar/app2/Program.cs
+++ Unidad02/Capitulo01/Lab01.ParaPensar/app2/Program.cs
@@ -1,2 +1,8 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+Console.WriteLine("Ingrese un año para validad si es bisiesto: ");
+int anio = int.Parse(Console.ReadLine() ?? "0");
+if (anio % 4 == 0 && anio % 100 != 0 || anio % 400 == 0)
+{
+    Console.WriteLine($"{anio} es bisiesto");
+}
+else Console.WriteLine($"{anio} no es bisiesto");
---
+++ Unidad02/Capitulo01/Lab01.ParaPensar/app3/Program.cs
@@ -0,0 +1,17 @@
+﻿// See https://aka.ms/new-console-template for more information
+using System.Diagnostics.CodeAnalysis;
+
+Console.WriteLine("Ingrese cuantas iteraciones de la serie de Fibonacci desea ver: ");
+int nros = int.Parse(Console.ReadLine() ?? "0");
+int a = 1;
+int b = 1;
+int aux;
+for (int i = 0; i < nros; i++)
+{
+    Console.WriteLine(a);
+    aux = a;
+    a = b;
+    b = b + aux;
+}
+
+
