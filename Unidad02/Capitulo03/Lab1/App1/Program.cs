7d93e6258b07e7c6748024dd20ea0b9201450d0f
pepicont <stefano.conti2004@gmail.com>
2025-05-12 14:13:13 -0300

    terminé app2 lab1SintaxisAvanzada, faltan last 2

---
+++ Unidad02/Capitulo03/Lab1/App1/Program.cs
@@ -0,0 +1,22 @@
+﻿// See https://aka.ms/new-console-template for more information
+//1) Dado un Array que incluya todas las Provincias de Argentina usar LINQ para obtener y mostrar
+//por Consola aquellas provincias que empiezan con la letra “S” o “T”
+using static System.Console;
+
+string[] provincias = new string[] {
+    "Buenos Aires", "Catamarca", "Chaco", "Chubut", "Córdoba", "Corrientes", "Entre Ríos",
+    "Formosa", "Jujuy", "La Pampa", "La Rioja", "Mendoza", "Misiones", "Neuquén", "Río Negro",
+    "Salta", "San Juan", "San Luis", "Santa Cruz", "Santa Fe", "Santiago del Estero", "Tierra del Fuego",
+    "Tucumán", "Ciudad Autónoma de Buenos Aires"
+};
+
+var provinciasSOT = provincias.Where(p => p.StartsWith("S") || p.StartsWith("T"));
+//sintaxis var=arreglo.Where(elemento => elemento.StartsWith("S") || ...)
+
+WriteLine("Provincias que empiezan con la letra S o T:\n");
+
+foreach (string provincia in provinciasSOT){
+    WriteLine($"{provincia}");
+}
+
+
---
+++ Unidad02/Capitulo03/Lab1/App2/Program.cs
@@ -0,0 +1,20 @@
+﻿// See https://aka.ms/new-console-template for more information
+//Crear un programa que acepte una lista de números, los almacene en un objeto del tipo List<int>
+//y luego usando LINQ muestre por Consola aquellos que son mayores a 20.
+var numero = "12";
+List<int> lista = new List<int>();
+while (numero != "exit") {
+    Console.WriteLine("Por favor, ingrese un número o \"exit\" para salir");
+    numero = Console.ReadLine();
+    if (numero != "exit") {
+        int numeroParse = int.Parse(numero);
+        lista.Add(numeroParse);
+    }
+}
+
+var numerosM20 = lista.Where(numero => numero > 20);
+
+Console.WriteLine("\nAquellos mayores a 20 son: ");
+foreach (int num in numerosM20) {
+    Console.WriteLine($"{num}");
+}
