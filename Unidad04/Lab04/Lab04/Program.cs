e2f980831547edd1a9b86d9707bef58edb855a54
pepicont <stefano.conti2004@gmail.com>
2025-09-22 08:54:09 -0300

    subido para que puedan ver EF

---
+++ Unidad04/Lab04/Lab04/Program.cs
@@ -0,0 +1,57 @@
+﻿// See https://aka.ms/new-console-template for more information
+using System;
+using System.Collections.Generic;
+using System.Text;
+using Lab04;
+
+namespace Lab04
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Contactos contactos = new ContactosMySQL();
+            contactos.listar();
+            menu(contactos);
+
+        }
+
+        static void menu(Contactos contactos)
+        {
+            string rta = "";
+            do
+            {
+                Console.WriteLine("1 - Listar");
+                Console.WriteLine("2 - Agregar");
+                Console.WriteLine("3 - Modificar");
+                Console.WriteLine("4 - Eliminar");
+                Console.WriteLine("5 - Guardar Cambios");
+                Console.WriteLine("6 - Salir");
+                rta = Console.ReadLine();
+                switch (rta)
+                {
+                    case "1":
+                        contactos.listar();
+                        break;
+                    case "2":
+                        contactos.nuevaFila();
+                        break;
+                    case "3":
+                        contactos.editarFila();
+                        break;
+                    case "4":
+                        contactos.eliminarFila();
+                        break;
+                    case "5":
+                        contactos.aplicaCambios();
+                        break;
+                    default:
+                        break;
+                }
+            } while (rta != "6");
+        }
+
+
+    }
+}
+
